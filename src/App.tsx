bae0dca03b4e2a099e8b8f2fc36b94e56fac7797
jones-bass <jonesbass.tb@gmail.com>
2024-10-30 23:34:35 -0300

    Add Effeito

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { Home } from './home';
+import { useEffect, useState } from 'react';
+import { LogoLoading } from './components/LogoLoading';
 
 
 export function App() {
+  const [isLogoLoading, setIsLogoLoading] = useState(true)
+
+  useEffect(() => {
+    const timingDelayInSeconds = 1000 * 4
+
+    const delay = setTimeout(() => {
+      setIsLogoLoading(false)
+    }, timingDelayInSeconds)
+
+    return () => {
+      clearTimeout(delay)
+    }
+  }, [])
+
   return (
     <>
-      <ToastContainer />
-      <Home />
+      {isLogoLoading ? (
+        <LogoLoading />
+      ) : (
+        <>
+          <ToastContainer />
+          <Home />
+        </>
+      )}
     </>
   );
 }
